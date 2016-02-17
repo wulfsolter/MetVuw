f459a1d042ba473cd7159585e40e364aebd16ead
Wulf Solter <wulf@wulf.co.nz>
2016-02-18 09:41:21 +1300

    Upgrade Ionic2 to 2.0.0-beta.0

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
   entry: [
     path.normalize('es6-shim/es6-shim.min'),
     'reflect-metadata',
-    'web-animations.min',
     path.normalize('zone.js/dist/zone-microtask'),
     path.resolve('app/app')
   ],
@@ -34,13 +33,13 @@ module.exports = {
     noParse: [
       /es6-shim/,
       /reflect-metadata/,
-      /web-animations/,
       /zone\.js(\/|\\)dist(\/|\\)zone-microtask/
     ]
   },
   resolve: {
     alias: {
-      'web-animations.min': path.normalize('ionic-framework/js/web-animations.min')
+      'angular2': path.resolve('node_modules/angular2'),
+      'ionic': 'ionic-framework',
     },
     extensions: ["", ".js", ".ts"]
   }
